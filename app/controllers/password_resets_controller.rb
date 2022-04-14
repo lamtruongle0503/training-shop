ac7f81924baef77c8b9344ab394f572d3e54305c
lamtruongle0503 <lamtruongle0503@gmail.com>
2022-04-14 14:02:10 +0700

    asdas

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -8,10 +8,10 @@ class PasswordResetsController < ApplicationController
   def create
     @user = User.find_by(email: params[:password_reset][:email].downcase)
     if @user
-      @user.create_reset_digest
-      @user.send_password_reset_email
-      flash[:info] = "Email sent with password reset instructions"
-      redirect_to root_url
+      # @user.create_reset_digest
+      # @user.send_password_reset_email
+      # flash[:info] = "Email sent with password reset instructions"
+      redirect_to edit_password_reset_url(@user.reset_token, email: @user.email)
     else
       flash.now[:danger] = "Email address not found"
       render 'new'
@@ -47,7 +47,7 @@ class PasswordResetsController < ApplicationController
   end
 
   def get_user
-  @user = User.find_by(email: params[:email])
+    @user = User.find_by(email: params[:email])
   end
   # Confirms a valid user.
   def valid_user
